941448cfa28de5643e4504f129cb42f05bd33eb4
Maria Miller <mariajmiller13@gmail.com>
2015-04-01 17:56:13 -0400

    Initial Commit

---
+++ 3d-robot/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  3d-robot
+//
+//  Created by Maria Miller on 4/1/15.
+//  Copyright (c) 2015 Maria Miller. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
