0958bc8e625a945006c4697cdcd55670a847b3d3
Maria Miller <mariajmiller13@gmail.com>
2015-04-03 21:21:57 -0400

    Show black glut window.

---
+++ 3d-robot/robot.h
@@ -0,0 +1,14 @@
+//
+//  robot.h
+//  3d-robot
+//
+//  Created by Maria Miller on 4/3/15.
+//  Copyright (c) 2015 Maria Miller. All rights reserved.
+//
+
+#ifndef ___d_robot__robot__
+#define ___d_robot__robot__
+
+#include <stdio.h>
+
+#endif /* defined(___d_robot__robot__) */
